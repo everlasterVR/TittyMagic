146e19be14ed7e2620ccd61f0adf9814457b6b82
everlaster <everlaster@protonmail.com>
2021-02-05 01:56:12 +0200

    Update package.sh to hide .cs files in archive

--- package.sh
+++ package.sh
@@ -4,15 +4,16 @@ resource_version=$1
 package_version=$2
 
 if [ -z "$resource_version" ]; then
-	echo "Resource version must be set."
-	exit 1
+    echo "Resource version must be set."
+    exit 1
 fi
 
 if [ -z "$package_version" ]; then
-	echo "Package version must be set."
-	exit 1
+    echo "Package version must be set."
+    exit 1
 fi
 
+# Setup archive contents
 resource_dir=publish/Custom/Scripts/everlaster/$resource_name/
 mkdir -p $resource_dir
 cp *.cslist $resource_dir
@@ -22,11 +23,18 @@ morphs_dir=publish/Custom/Atom/Person/Morphs/female/everlaster/
 mkdir -p $morphs_dir
 cp -r /mnt/e/VaM/Custom/Atom/Person/Morphs/female/everlaster/$resource_name $morphs_dir
 
+# Update version info in meta.json
 cp meta.json publish/
-
 sed -i "s/v0.0.0/$resource_version/" publish/meta.json
 
 cd publish
+
+# hide .cs files (plugin is loaded with .cslist)
+for file in $(find . -type f -name "*.cs"); do
+    touch $file.hide
+done
+
+# Zip files to .var and cleanup
 package="everlaster.$resource_name.$package_version.var"
 zip -r $package *
 cd ..
