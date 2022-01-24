521b7d0ad61f86294b358f031f2f1769145033fe
everlaster <everlaster@protonmail.com>
2022-01-24 09:52:49 +0200

    decrease breast morph listener filter strength to better recognize body morphs, exclude specific non body morphs

--- src/BreastMorphListener.cs
+++ src/BreastMorphListener.cs
@@ -1,4 +1,4 @@
-﻿//#define SHOW_DEBUG
+﻿//#define DEBUG_ON
 
 using System;
 using System.Collections.Generic;
@@ -10,14 +10,35 @@ namespace TittyMagic
     internal class BreastMorphListener
     {
         // does not include areola/nipple vertices
-        private HashSet<int> breastVertices = new HashSet<int>()
+        private HashSet<int> breastVertices = new HashSet<int>
         {
             8, 16, 17, 93, 135, 136, 137, 138, 2401, 2402, 2403, 2404, 2405, 2406, 2407, 2408, 2409, 2410, 2411, 2412, 2413, 2414, 2415, 2416, 2417, 2418, 2419, 2590, 2591, 2592, 2594, 2596, 2597, 2598, 2871, 2872, 2873, 7216, 7218, 7219, 7220, 7230, 7231, 7232, 7233, 7234, 7235, 7236, 7237, 7238, 7239, 7240, 7241, 7242, 7243, 7244, 7245, 7246, 7247, 7248, 8833, 8834, 8836, 8837, 8838, 8839, 8840, 8848, 8849, 8850, 8851, 8852, 8853, 8854, 10936, 10944, 10945, 11021, 11063, 11064, 11065, 11066, 13231, 13232, 13233, 13234, 13235, 13236, 13237, 13238, 13239, 13240, 13241, 13242, 13243, 13244, 13245, 13246, 13247, 13248, 13249, 13408, 13409, 13410, 13411, 13412, 13413, 13414, 13672, 13673, 13674, 17923, 17924, 17925, 17926, 17936, 17937, 17938, 17939, 17940, 17941, 17942, 17943, 17944, 17945, 17946, 17947, 17948, 17949, 17950, 17951, 17952, 17953, 17954, 19504, 19505, 19507, 19508, 19509, 19510, 19511, 19519, 19520, 19521, 19522, 19523, 19524, 19525, 19639
         };
 
-        private static float filterStrength = 0.009f;
+        private List<string> excludeMorphs = new List<string>
+        {
+            "FBMFitnessDetails",
+            "FBMHeight",
+            "PBMShouldersSize",
+            "Alessa",
+            "Eisa",
+            "Izarra",
+            "PBMLegsLength",
+            "Shin Length",
+            "Thigh Length",
+            "Upper Body Length",
+            "G3F_Hip Abdomen Shape",
+            "bellybulge",
+            "deepthroat",
+            "Athletic",
+            "BodyShort",
+            "Hollow back",
+        };
+
+        private static float filterStrength = 0.005f;
 
         private HashSet<DAZMorph> listenedMorphs = new HashSet<DAZMorph>();
+
         private Dictionary<string, float> status = new Dictionary<string, float>();
 
         public BreastMorphListener(List<DAZMorph> morphs)
@@ -26,7 +47,7 @@ namespace TittyMagic
             {
                 try
                 {
-                    if(!morph.visible || morph.isPoseControl || morph.group.Contains("Pose/"))
+                    if(!morph.visible || morph.isPoseControl || morph.group.Contains("Pose/") || excludeMorphs.Contains(morph.morphName))
                     {
                         continue;
                     }
@@ -39,12 +60,13 @@ namespace TittyMagic
                 }
                 catch(Exception)
                 {
-#if SHOW_DEBUG
+#if DEBUG_ON
                     LogMessage($"Unable to initialize listener for morph {morph.morphName}.", nameof(BreastMorphListener));
 #endif
                 }
             }
-#if SHOW_DEBUG
+
+#if DEBUG_ON
             LogMessage(GetStatus() + "- - - - - - - - - -\n", nameof(BreastMorphListener));
 #endif
         }
@@ -57,7 +79,7 @@ namespace TittyMagic
                 if(value != status[morph.uid])
                 {
                     status[morph.uid] = value;
-#if SHOW_DEBUG
+#if DEBUG_ON
                     LogMessage($"change detected! morph {MorphName(morph)}", nameof(BreastMorphListener));
 #endif
                     return true;
@@ -88,7 +110,7 @@ namespace TittyMagic
             return false;
         }
 
-#if SHOW_DEBUG
+#if DEBUG_ON
 
         public string GetStatus()
         {
