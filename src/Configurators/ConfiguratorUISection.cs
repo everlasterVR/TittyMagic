9d764f7ad164a9023c57287a3c91f37576efd446
everlaster <everlaster@protonmail.com>
2022-02-17 17:16:17 +0200

    fix ConfiguratorUISection's use of UI functions

--- src/Configurators/ConfiguratorUISection.cs
+++ src/Configurators/ConfiguratorUISection.cs
@@ -12,20 +12,20 @@
         public ConfiguratorUISection(MVRScript script, MorphConfig config)
         {
             Name = config.Name;
-            UI.NewTextField(script, Name, $"\n{Name}", 32);
+            script.NewTextField(Name, $"\n{Name}", 32);
 
             IsNegativeStorable = new JSONStorableBool("IsNegative", config.IsNegative);
 
-            Multiplier1Storable = UI.NewFloatSlider(script, "Multiplier1", config.Multiplier1, -3, 3, "F3", true);
+            Multiplier1Storable = script.NewFloatSlider("Multiplier1", config.Multiplier1, -3, 3, "F3", true);
             Multiplier1Storable.slider.onValueChanged.AddListener((float val) =>
             {
                 config.Multiplier1 = val;
             });
 
-            ValueStorable = UI.NewFloatSlider(script, "value", config.Morph.morphValue, 2 * config.Morph.min, 2 * config.Morph.max, "F3");
+            ValueStorable = script.NewFloatSlider("value", config.Morph.morphValue, 2 * config.Morph.min, 2 * config.Morph.max, "F3");
             ValueStorable.slider.interactable = false;
 
-            Multiplier2Storable = UI.NewFloatSlider(script, "Multiplier2", config.Multiplier2, -3, 3, "F3", true);
+            Multiplier2Storable = script.NewFloatSlider("Multiplier2", config.Multiplier2, -3, 3, "F3", true);
             Multiplier2Storable.slider.onValueChanged.AddListener((float val) =>
             {
                 config.Multiplier2 = val;
@@ -35,21 +35,21 @@
         public ConfiguratorUISection(MVRScript script, GravityPhysicsConfig config)
         {
             Name = config.Name;
-            UI.NewTextField(script, Name, $"\n{Name}", 32);
+            script.NewTextField(Name, $"\n{Name}", 32);
 
             TypeStorable = new JSONStorableString("Type", config.Type);
             IsNegativeStorable = new JSONStorableBool("IsNegative", config.IsNegative);
 
-            Multiplier1Storable = UI.NewFloatSlider(script, "Multiplier1", config.Multiplier1, -40, 40, "F2", true);
+            Multiplier1Storable = script.NewFloatSlider("Multiplier1", config.Multiplier1, -40, 40, "F2", true);
             Multiplier1Storable.slider.onValueChanged.AddListener((float val) =>
             {
                 config.Multiplier1 = val;
             });
 
-            ValueStorable = UI.NewFloatSlider(script, "value", config.Setting.val, config.Setting.min, config.Setting.max, "F2");
+            ValueStorable = script.NewFloatSlider("value", config.Setting.val, config.Setting.min, config.Setting.max, "F2");
             ValueStorable.slider.interactable = false;
 
-            Multiplier2Storable = UI.NewFloatSlider(script, "Multiplier2", config.Multiplier2, -40, 40, "F2", true);
+            Multiplier2Storable = script.NewFloatSlider("Multiplier2", config.Multiplier2, -40, 40, "F2", true);
             Multiplier2Storable.slider.onValueChanged.AddListener((float val) =>
             {
                 config.Multiplier2 = val;
