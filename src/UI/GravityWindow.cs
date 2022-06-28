a9edb40265668e9fc20e4d13119e3fd3e9801cb3
everlaster <everlaster@protonmail.com>
2022-06-28 03:34:58 +0300

    move gravity offset morphing to gravity tab, rename gravity and morphing sliders

--- src/UI/GravityWindow.cs
+++ src/UI/GravityWindow.cs
@@ -9,6 +9,8 @@ namespace TittyMagic.UI
 
         private readonly JSONStorableString _gravityPhysicsMultipliersHeader;
         private readonly JSONStorableString _gravityPhysicsMultipliersInfoText;
+        private readonly JSONStorableString _offsetMorphingInfoText;
+        private readonly JSONStorableString _otherSettingsHeader;
 
         public int Id() => 4;
 
@@ -18,12 +20,17 @@ namespace TittyMagic.UI
 
             _gravityPhysicsMultipliersHeader = new JSONStorableString("gravityPhysicsMultipliersHeader", "");
             _gravityPhysicsMultipliersInfoText = new JSONStorableString("gravityPhysicsMultipliersInfoText", "");
+            _otherSettingsHeader = new JSONStorableString("otherSettingsHeader", "");
+            _offsetMorphingInfoText = new JSONStorableString("offsetMorphingInfoText", "");
 
             _gravityPhysicsMultipliersInfoText.val = "\n".Size(12) +
                 "Adjust the effect of chest angle on breast main physics settings." +
                 "\n\n" +
                 "Higher values mean breasts drop more heavily up/down and left/right, " +
                 "are more swingy when leaning forward, and less springy when leaning back.";
+
+            _offsetMorphingInfoText.val = "\n".Size(12) +
+                "Rotate breasts up when upright to compensate for negative Up/Down Angle Target.";
         }
 
         public void Rebuild()
@@ -31,10 +38,14 @@ namespace TittyMagic.UI
             elements = new Dictionary<string, UIDynamic>();
 
             CreateHeader(_gravityPhysicsMultipliersHeader, "Gravity Physics Multipliers", false);
-            CreateMultiplierSlider(_script.gravityYStorable, "Physics Up/Down", false);
-            CreateMultiplierSlider(_script.gravityXStorable, "Physics Left/Right", false);
-            CreateMultiplierSlider(_script.gravityZStorable, "Physics Forward/Back", false);
+            CreateMultiplierSlider(_script.gravityYStorable, "Up/Down", false);
+            CreateMultiplierSlider(_script.gravityXStorable, "Left/Right", false);
+            CreateMultiplierSlider(_script.gravityZStorable, "Forward/Back", false);
             CreateGravityPhysicsInfoTextArea(true, spacing: 62);
+
+            CreateHeader(_otherSettingsHeader, "Other", false);
+            CreateOffsetMorphingSlider(false);
+            CreateOffsetMorphingInfoTextArea(true, spacing: 62);
         }
 
         private void CreateHeader(JSONStorableString storable, string text, bool rightSide)
@@ -63,6 +74,29 @@ namespace TittyMagic.UI
             elements[storable.name] = textField;
         }
 
+        private void CreateOffsetMorphingSlider(bool rightSide, int spacing = 0)
+        {
+            var storable = _script.offsetMorphing;
+            AddSpacer(storable.name, spacing, rightSide);
+
+            var slider = _script.CreateSlider(storable, rightSide);
+            slider.valueFormat = "F2";
+            slider.label = "Gravity Offset Morphing";
+            slider.AddSliderClickMonitor();
+            elements[storable.name] = slider;
+        }
+
+        private void CreateOffsetMorphingInfoTextArea(bool rightSide, int spacing = 0)
+        {
+            var storable = _offsetMorphingInfoText;
+            AddSpacer(storable.name, spacing, rightSide);
+
+            var textField = _script.CreateTextField(storable, rightSide);
+            textField.UItext.fontSize = 28;
+            textField.height = 115;
+            elements[storable.name] = textField;
+        }
+
         private void AddSpacer(string name, int height, bool rightSide)
         {
             elements[$"{name}Spacer"] = _script.NewSpacer(height, rightSide);
@@ -76,6 +110,7 @@ namespace TittyMagic.UI
                 sliders.Add(elements[_script.gravityYStorable.name] as UIDynamicSlider);
                 sliders.Add(elements[_script.gravityXStorable.name] as UIDynamicSlider);
                 sliders.Add(elements[_script.gravityZStorable.name] as UIDynamicSlider);
+                sliders.Add(elements[_script.offsetMorphing.name] as UIDynamicSlider);
             }
 
             return sliders;
--- src/UI/MorphingWindow.cs
+++ src/UI/MorphingWindow.cs
@@ -9,8 +9,7 @@ namespace TittyMagic.UI
 
         private readonly JSONStorableString _dynamicMorphingMultipliersHeader;
         private readonly JSONStorableString _dynamicMorphingMultipliersInfoText;
-        private readonly JSONStorableString _additionalSettingsHeader;
-        private readonly JSONStorableString _offsetMorphingInfoText;
+        private readonly JSONStorableString _otherSettingsHeader;
 
         public int Id() => 3;
 
@@ -20,16 +19,12 @@ namespace TittyMagic.UI
 
             _dynamicMorphingMultipliersHeader = new JSONStorableString("dynamicMorphingMultipliersHeader", "");
             _dynamicMorphingMultipliersInfoText = new JSONStorableString("dynamicMorphingMultipliersInfoText", "");
-            _additionalSettingsHeader = new JSONStorableString("additionalSettingsHeader", "");
-            _offsetMorphingInfoText = new JSONStorableString("offsetMorphingInfoText", "");
+            _otherSettingsHeader = new JSONStorableString("otherSettingsHeader", "");
 
             _dynamicMorphingMultipliersInfoText.val = "\n".Size(12) +
                 "Adjust the amount of breast morphing due to forces including gravity.\n" +
                 "\n" +
                 "Breasts morph up/down, left/right and forward/back.";
-
-            _offsetMorphingInfoText.val = "\n".Size(12) +
-                "Rotate breasts up when upright to compensate for negative Up/Down Angle Target.";
         }
 
         public void Rebuild()
@@ -37,15 +32,13 @@ namespace TittyMagic.UI
             elements = new Dictionary<string, UIDynamic>();
 
             CreateHeader(_dynamicMorphingMultipliersHeader, "Dynamic Morphing Multipliers", false);
-            CreateMultiplierSlider(_script.morphingYStorable, "Morphing Up/Down", false);
-            CreateMultiplierSlider(_script.morphingXStorable, "Morphing Left/Right", false);
-            CreateMultiplierSlider(_script.morphingZStorable, "Morphing Forward/Back", false);
+            CreateMultiplierSlider(_script.morphingYStorable, "Up/Down", false);
+            CreateMultiplierSlider(_script.morphingXStorable, "Left/Right", false);
+            CreateMultiplierSlider(_script.morphingZStorable, "Forward/Back", false);
             CreateDynamicMorphingInfoTextArea(true, spacing: 62);
 
-            CreateHeader(_additionalSettingsHeader, "Additional Settings", false);
-            CreateOffsetMorphingSlider(false);
+            CreateHeader(_otherSettingsHeader, "Other", false);
             CreateNippleErectionSlider(false);
-            CreateOffsetMorphingInfoTextArea(true, spacing: 62);
         }
 
         private void CreateHeader(JSONStorableString storable, string text, bool rightSide)
@@ -60,18 +53,6 @@ namespace TittyMagic.UI
             var slider = _script.CreateSlider(storable, rightSide);
             slider.valueFormat = "F2";
             slider.label = label;
-            slider.AddSliderClickMonitor();
-            elements[storable.name] = slider;
-        }
-
-        private void CreateOffsetMorphingSlider(bool rightSide, int spacing = 0)
-        {
-            var storable = _script.offsetMorphing;
-            AddSpacer(storable.name, spacing, rightSide);
-
-            var slider = _script.CreateSlider(storable, rightSide);
-            slider.valueFormat = "F2";
-            slider.label = "Gravity Offset Morphing";
             slider.AddSliderClickMonitor();
             elements[storable.name] = slider;
         }
@@ -98,17 +79,6 @@ namespace TittyMagic.UI
             elements[storable.name] = textField;
         }
 
-        private void CreateOffsetMorphingInfoTextArea(bool rightSide, int spacing = 0)
-        {
-            var storable = _offsetMorphingInfoText;
-            AddSpacer(storable.name, spacing, rightSide);
-
-            var textField = _script.CreateTextField(storable, rightSide);
-            textField.UItext.fontSize = 28;
-            textField.height = 115;
-            elements[storable.name] = textField;
-        }
-
         private void AddSpacer(string name, int height, bool rightSide)
         {
             elements[$"{name}Spacer"] = _script.NewSpacer(height, rightSide);
@@ -122,7 +92,6 @@ namespace TittyMagic.UI
                 sliders.Add(elements[_script.morphingXStorable.name] as UIDynamicSlider);
                 sliders.Add(elements[_script.morphingYStorable.name] as UIDynamicSlider);
                 sliders.Add(elements[_script.morphingZStorable.name] as UIDynamicSlider);
-                sliders.Add(elements[_script.offsetMorphing.name] as UIDynamicSlider);
                 sliders.Add(elements[_script.nippleErection.name] as UIDynamicSlider);
             }
 
