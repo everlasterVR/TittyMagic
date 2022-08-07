3a49fc2eb494ca896da961f6c64cf7aa8ae5481b
everlaster <everlaster@protonmail.com>
2022-08-07 20:00:59 +0300

    update packaging script

--- package.sh
+++ package.sh
@@ -1,55 +1,64 @@
 #!/bin/bash
+
 creator_name=everlaster
-plugin_name=TittyMagic
+resource_name=TittyMagic
 package_version=$1
-plugin_version=$2
+plugin_version=$(git tag --points-at HEAD)
 
-usage()
-{
-    echo "Usage: ./package.sh <VarPackageVersion> <PluginVersion>"
-    echo "e.g.   ./package.sh 1 1.0.0"
-    exit 1
-}
+[ -z "$package_version" ] && printf "Usage: ./package.sh [var package version]\n" && exit 1
+[ -z "$plugin_version" ] && printf "Git tag not set on current commit.\n" && exit 1
 
-[ -z "$package_version" ] && usage
-[ -z "$plugin_version" ] && usage
+# setup archive contents
+work_dir=publish
+mkdir -p work_dir
 
-# Setup archive contents
-publish_dir=publish/Custom/Scripts/$creator_name/$plugin_name
-mkdir -p $publish_dir
-cp meta.json publish/
-cp *.cslist $publish_dir/
-cp -r src $publish_dir/
+resource_dir=$work_dir/Custom/Scripts/$creator_name/$resource_name
+mkdir -p $resource_dir
+cp meta.json $work_dir/
+cp *.cslist $resource_dir/
+cp -r src $resource_dir/
 
-# Additional packaging
-mkdir -p $publish_dir/vam-collider-editor
-cp -r vam-collider-editor/src $publish_dir/vam-collider-editor
-cp -r vam-collider-editor/*.md $publish_dir/vam-collider-editor
+# additional packaging
+mkdir -p $resource_dir/vam-collider-editor
+cp -r vam-collider-editor/src $resource_dir/vam-collider-editor
+cp -r vam-collider-editor/*.md $resource_dir/vam-collider-editor
 
-mkdir -p $publish_dir/settings
-cp -r settings/morphmultipliers $publish_dir/settings/
+mkdir -p $resource_dir/settings
+cp -r settings/morphmultipliers $resource_dir/settings/
 
-morphs_dir=publish/Custom/Atom/Person/Morphs/female/$creator_name/$plugin_name
+morphs_dir=$work_dir/Custom/Atom/Person/Morphs/female/$creator_name/$resource_name
 mkdir -p $morphs_dir
 cp -r local/Morphs/* $morphs_dir/
 
-# Update version info
-sed -i "s/v0\.0\.0/v$plugin_version/g" publish/meta.json
-sed -i "s/v0\.0\.0/v$plugin_version/g" $publish_dir/src/Script.cs
-sed -i "s/#define/\/\/#define/g" $publish_dir/src/Script.cs
+# update version info
+sed -i "s/v0\.0\.0/v$plugin_version/g" $work_dir/meta.json
+sed -i "s/v0\.0\.0/v$plugin_version/g" $resource_dir/src/Script.cs
+sed -i "s/#define/\/\/#define/g" $resource_dir/src/Script.cs
 
 # hide .cs files (plugin is loaded with .cslist)
-for file in $(find $publish_dir -type f -name "*.cs"); do
+for file in $(find $resource_dir -type f -name "*.cs"); do
     touch $file.hide
 done
 
-# Zip files to .var and cleanup
-cd publish
-package="$creator_name.$plugin_name.$package_version.var"
-zip -r $package *
+# zip files to .var and cleanup
+printf "Creating package...\n"
+package_file="$creator_name.$resource_name.$package_version.var"
+cd $work_dir
+zip -rq $package_file *
+printf "Package $package_file created for plugin version $plugin_version.\n"
+mv $package_file ..
 cd ..
-mkdir -p ../../../../AddonPackages/Self
-mv publish/$package ../../../../AddonPackages/Self
-rm -rf publish
+rm -rf $work_dir
 
-echo "Package $package created with version $plugin_version and moved to AddonPackages/Self."
+# upload
+if ../scripts/mega-login.sh && ../scripts/mega-uploadandshare.sh $package_file; then
+    printf "Upload successful.\n"
+else
+    printf "Upload failed.\n"
+fi;
+
+# move archive to AddonPackages
+addon_packages_dir=../../../../AddonPackages/Self
+mkdir -p $addon_packages_dir
+mv $package_file $addon_packages_dir
+printf "Package moved to AddonPackages/Self.\n".
